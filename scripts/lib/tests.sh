14dddf8463137c21dca5e6c4d6f208213a0982e5
Jay Deokar <jsdeokar@amazon.com>
2024-05-08 22:34:18 -0700

    Update test script to fail when verification step fails

--- scripts/lib/tests.sh
+++ scripts/lib/tests.sh
@@ -6,15 +6,15 @@ function run_cyclonus_tests(){
     kubectl apply -f ${DIR}/test/cyclonus-config.yaml -n netpol
 
     echo "Executing cyclonus suite"
-    kubectl wait --for=condition=complete --timeout=240m -n netpol job.batch/cyclonus || echo "Job timed out after 4 hrs"
+    kubectl wait --for=condition=complete --timeout=240m -n netpol job.batch/cyclonus || TEST_FAILED=true
     kubectl logs -n netpol job/cyclonus > ${DIR}/results.log
 
     # Cleanup after test finishes
     kubectl delete clusterrolebinding cyclonus
-    kubectl delete ns netpol
+    kubectl delete ns netpol x y z
 
     cat ${DIR}/results.log
 
     echo "Verify results against expected"
-    python3 ${DIR}/lib/verify_test_results.py -f ${DIR}/results.log -ip $IP_FAMILY || (echo "Cyclonus tests have failed" && TEST_FAILED=true)
+    python3 ${DIR}/lib/verify_test_results.py -f ${DIR}/results.log -ip $IP_FAMILY || TEST_FAILED=true
 }
